e33c3c9142c4da06edb9b1d5a02dbbe8aef244f8
chainsawriot <chainsawtiney@gmail.com>
2020-06-17 00:43:12 +0200

    complete darkmode

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A client-side enhancement to CRAN.
 
+![](cranitup.gif)
+
 1. Display download statistic
 2. Display age of the latest version
 3. Display install.packages command and copy button
+4. Dark mode!
+
+# Installation
+
+1. Install [Tampermonkey](https://www.tampermonkey.net/) (Firefox or Chrome)
+2. Click [this link](https://raw.githubusercontent.com/chainsawriot/cranitup/master/cranitup.user.js) to install the userscript
+
--- cranitup.user.js
+++ cranitup.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         cranitup
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
-// @description  try to take over the world!
+// @version      0.2.0
+// @description  Make CRAN (Comprehensive R Archive Network) more useful
 // @author       Chung-hong Chan
 // @include      https://*.r-project.org/web/packages/*
 // @exclude      https://*.r-project.org/web/packages/available_packages_by_name.html
@@ -19,6 +19,16 @@
     //script.type = 'text/javascript';
     //script.src = 'https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js'
     //document.head.appendChild(script);
+    GM_addStyle("@media (prefers-color-scheme: light) { :root { --background-color: white; --text-color: black; --link-color: blue; --sec-color: rgb(55%, 55%, 55%); } }")
+    GM_addStyle("@media (prefers-color-scheme: dark) { :root { --background-color: #121212; --text-color: white; --link-color: #BB86FC; --sec-color: #03DAC5; } }")
+    GM_addStyle("body { background: var(--background-color); color: var(--text-color); }")
+    GM_addStyle("h1 { background: var(--background-color); color: var(--sec-color); font-family: monospace; font-size: x-large; text-align: center;}")
+    GM_addStyle("a { background: var(--background-color); color: var(--link-color); }")
+    GM_addStyle("h1 { background: var(--background-color); color: var(--sec-color); font-family: monospace; font-size: x-large; text-align: center; }")
+    GM_addStyle("h2 { background: var(--background-color); color: var(--sec-color); font-family: monospace; font-size: large; }")
+    GM_addStyle("h3, h4, h5 { background: var(--background-color); color: var(--sec-color); font-family: monospace; }")
+    GM_addStyle("button { background-color: var(--sec-color); color: var(--text-color); }")
+    GM_addStyle("input[type=text] { background-color: var(--background-color); color: var(--sec-color); }")
     var pkg_name = document.querySelector('h2').textContent.split(':')[0]
     var download_badge = document.createElement("img")
     download_badge.src = "https://cranlogs.r-pkg.org/badges/last-week/" + pkg_name
@@ -42,9 +52,4 @@
     input_box.value = "install.packages(\"" + pkg_name + "\", dependencies = TRUE)"
     input_box.size = 80
     document.querySelector('p').append(br, br, input_box, copy_button)
-    GM_addStyle("@media (prefers-color-scheme: light) { :root { --background-color: white; --text-color: black; --link-color: blue; --sec-color: rgb(55%, 55%, 55%); } }")
-    GM_addStyle("@media (prefers-color-scheme: dark) { :root { --background-color: #121212; --text-color: white; --link-color: #BB86FC; --sec-color: #3700B7; } }")
-    GM_addStyle("body { background: var(--background-color); color: var(--text-color); }")
-    GM_addStyle("h1 { background: var(--background-color); color: var(--sec-color); font-family: monospace; font-size: x-large; text-align: center;}")
-    GM_addStyle("a { background: var(--background-color); color: var(--link-color); }")
 })();
